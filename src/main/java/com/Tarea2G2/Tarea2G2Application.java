7b118742b3b5601292e19f32c6ad5f944e8a4793
manul <manul@DESKTOP-KCC0TG9>
2023-02-07 17:12:33 -0600

    Tarea 2 sobre G2 Esports

---
+++ src/main/java/com/Tarea2G2/Tarea2G2Application.java
@@ -0,0 +1,13 @@
+package com.Tarea2G2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea2G2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea2G2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tarea2G2/Tarea2G2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tarea2G2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea2G2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
