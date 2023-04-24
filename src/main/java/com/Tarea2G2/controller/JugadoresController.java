81fd280ac2099a1f3436b411c0c5ec0dcce231fc
manul <manul@DESKTOP-KCC0TG9>
2023-04-24 15:49:34 -0600

    jugadores arreglo

--- src/main/java/com/Tarea2G2/controller/JugadoresController.java
+++ src/main/java/com/Tarea2G2/controller/JugadoresController.java
@@ -1,12 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.Tarea2G2.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-public class jugadores {
+/**
+ *
+ * @author manul
+ */
+public class JugadoresController {
 
     @GetMapping(value = "/jugadores")
     public ModelAndView jugadoresPage() {
