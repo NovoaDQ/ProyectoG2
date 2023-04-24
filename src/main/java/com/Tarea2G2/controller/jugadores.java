7ac01f05a0b768e10bf9ff52efb663284bfc551f
manul <manul@DESKTOP-KCC0TG9>
2023-04-24 12:28:07 -0600

    Ultimo avance de G2

---
+++ src/main/java/com/Tarea2G2/controller/jugadores.java
@@ -0,0 +1,15 @@
+package com.Tarea2G2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class jugadores {
+
+    @RequestMapping(value = "/jugadores")
+    public ModelAndView jugadoresPage() {
+        ModelAndView mav = new ModelAndView("jugadores");
+        return mav;
+    }
+}
