960eb8f6c07cf5d380ac54a4a767bf189a0240a7
manul <manul@DESKTOP-KCC0TG9>
2023-04-24 19:04:47 -0600

    arreglo del service imp x2

--- src/main/java/com/Tarea2G2/service/ClienteService.java
+++ src/main/java/com/Tarea2G2/service/ClienteService.java
@@ -3,20 +3,14 @@ package com.Tarea2G2.service;
 import com.Tarea2G2.domain.Cliente;
 import java.util.List;
 
-
 public interface ClienteService {
-      public List<Cliente> getClientes();
-    
+
+    public List<Cliente> getClientes();
+
     public Cliente getCliente(Cliente cliente);
-    
-    public void save (Cliente cliente);
-    
-    public void delete (Cliente cliente);
-    
-    public List<Cliente> getClienteCorreo(String correo);
-    
-    public List<Cliente> getClienteNombreApellidos(String nombre, String apellidos);
-    
-    public List<Cliente> getClienteNombre(String nombre);
+
+    public void save(Cliente cliente);
+
+    public void delete(Cliente cliente);
 
 }
--- src/main/java/com/Tarea2G2/service/ClienteSeviceImp.java
+++ src/main/java/com/Tarea2G2/service/ClienteSeviceImp.java
@@ -1,19 +1,25 @@
 package com.Tarea2G2.service;
 
+import com.Tarea2G2.dao.ClienteDao;
 import com.Tarea2G2.domain.Cliente;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ *
+ * @author manul
+ */
 @Service
-public class ClienteServiceImpl implements ClienteService {
+public class ClienteSeviceImp implements ClienteService {
 
     @Autowired
     ClienteDao clienteDao;
+    
 
     @Override
-    @Transactional(readOnly = true) 
+    @Transactional(readOnly = true) // para manejar transacciones de solo lectura
     public List<Cliente> getClientes() {
         return (List<Cliente>) clienteDao.findAll();
     }
@@ -23,7 +29,6 @@ public class ClienteServiceImpl implements ClienteService {
     public Cliente getCliente(Cliente cliente) {
         return clienteDao.findById(cliente.getIdCliente()).orElse(null);
     }
-
     @Override
     @Transactional
     public void delete(Cliente cliente) {
@@ -31,24 +36,9 @@ public class ClienteServiceImpl implements ClienteService {
     }
 
     @Override
-    public List<Cliente> getClienteCorreo(String correo) {
-        return (List<Cliente>) clienteDao.findByCorreo(correo);
-    }
-
-    @Override
-    public List<Cliente> getClienteNombreApellidos(String nombre, String apellidos) {
-        return (List<Cliente>) clienteDao.findByNombreOrApellidos(nombre, apellidos);
-    }
-
-    @Override
-    public List<Cliente> getClienteNombre(String nombre) {
-        return (List<Cliente>) clienteDao.findByNombre(nombre);
-    }
-
-    @Override
-    @Transactional
     public void save(Cliente cliente) {
-        clienteDao.save(cliente);
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
+
 
 }
