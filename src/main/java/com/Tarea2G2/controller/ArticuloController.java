156b95095ef702a7ee860c26eff7a42c0445c773
manul <manul@DESKTOP-KCC0TG9>
2023-04-25 14:10:27 -0600

    articulo controller

--- src/main/java/com/Tarea2G2/controller/ArticuloController.java
+++ src/main/java/com/Tarea2G2/controller/ArticuloController.java
@@ -4,9 +4,13 @@
  */
 package com.Tarea2G2.controller;
 
+import com.Tarea2G2.domain.Articulo;
+import com.Tarea2G2.service.ArticuloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -16,9 +20,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ArticuloController {
 
+      @Autowired
+    ArticuloService articuloService;
+    
+
     @GetMapping("/articulo/listado")
-    public String articulo() {
-        return "articulo";
+    public String inicio(Model model) {
+        var articulos = articuloService.getArticulos(false);
+
+        model.addAttribute("articulos", articulos);
+
+        return "/articulo/listado";
+    }
+
+    @PostMapping("/articulo/guardar")
+    public String guardarArticulo(Articulo articulo) {
+        articuloService.save(articulo);
+        return "redirect:/articulo/listado";
+    }
+
+    @GetMapping("/articulo/modificar/{idArticulo}")
+    public String modificarArticulo(Articulo articulo, Model model) {
+        articulo = articuloService.getArticulo(articulo);
+        model.addAttribute("articulo", articulo);
+        return "/articulo/modificar";
+    }
+
+    @GetMapping("/articulo/eliminar/{idArticulo}")
+    public String eliminarArticulo(Articulo articulo) {
+        articuloService.delete(articulo);
+        return "redirect:/articulo/listado";
     }
 
 }
